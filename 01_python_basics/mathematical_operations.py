6310ddf25958e1f491cc9d1d831e2d31643d2f74
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-28 08:35:27 +0530

    mathematical_operations

---
+++ 01_python_basics/mathematical_operations.py
@@ -0,0 +1,16 @@
+print(3 + 5) # 8
+print(7 - 4) # 3
+print(3 * 2) # 6
+print(6 / 3) # 2.0
+print(type(6 / 3))  # <class 'float'>
+
+print(2 ** 6) # 64
+
+# use PEMDASLR ---- LR - left to Right method
+# ()   
+# **
+# * /
+# + -
+
+print(3 * 3 + 3 / 3 - 3) # 7.0
+print(3 * (3 + 3) / 3 - 3) # 3.0
