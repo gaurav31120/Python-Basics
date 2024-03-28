62e7afa570f52d914813e80b09ff2f7ea301a99d
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-28 07:26:22 +0530

    first commit

---
+++ 01_python_basics/01_datatypes.py
@@ -0,0 +1,24 @@
+# Data Types
+
+#Stringd
+
+print("Hello"[0])  # H
+print("Hello"[4])  # o
+
+print("123" + "456") #123456
+
+#Integer
+
+print(123 + 345) #468
+
+# print(123_456_789) 
+
+# Float 
+# 3.1234
+# Float ing point number
+# 312.34
+
+# Boolean
+
+# True
+# False
